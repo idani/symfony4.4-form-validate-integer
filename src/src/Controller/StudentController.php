fe5f066d97176fe38f546297d73b598bb548f4db
idani <idani@hirotae.com>
2019-12-23 00:36:21 +0900

    フォームのバリデーションの機能を追加

--- src/src/Controller/StudentController.php
+++ src/src/Controller/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Student;
 use App\Form\StudentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StudentController extends AbstractController
@@ -12,10 +13,15 @@ class StudentController extends AbstractController
     /**
      * @Route("/student", name="student")
      */
-    public function index()
+    public function index(Request $request)
     {
         $student = new Student();
         $form = $this->createForm(StudentType::class, $student);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+        }
 
         return $this->render('student/index.html.twig', [
             'controller_name' => 'StudentController',
