c071ed7a3126f0cde8c6dbc0eae086ebcf252f7c
idani <idani@hirotae.com>
2019-12-22 22:27:22 +0900

    Student エンティティを作成

---
+++ src/src/Entity/Student.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\StudentRepository")
+ */
+class Student
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $studentNo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStudentNo(): ?int
+    {
+        return $this->studentNo;
+    }
+
+    public function setStudentNo(int $studentNo): self
+    {
+        $this->studentNo = $studentNo;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+}
